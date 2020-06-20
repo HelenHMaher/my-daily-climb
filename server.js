28565d567aa309c69bb7d3b94b807d6ac8ab4f22
HelenHMaher <helenharlanmaher@gmail.com>
2020-06-20 16:20:09 +0200

    initial set-up with heroku

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const favicon = require("express-favicon");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(favicon(__dirname + "/build/HMlogo.gif"));
+app.use("/", express.static(path.join(__dirname, "build")));
+app.get("/heartbeath", function (req, res) {
+  res.send("<3");
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
