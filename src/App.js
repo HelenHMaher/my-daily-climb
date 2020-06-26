27e2b1f1065cb9c33e00a79455987f14dfef13e3
HelenHMaher <helenharlanmaher@gmail.com>
2020-06-26 16:27:01 +0200

    login screen and auto deploy test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ function App() {
     <div className="App">
       <header className="App-header">My Daily Climb</header>
       <p>tracks the daily training and fitness for rock climbers</p>
+      <form className="login" action="/login" method="POST">
+        <div className="inputField">
+          <label>Username:</label>
+          <input type="text" name="username" />
+        </div>
+        <div className="inputField">
+          <label>Password:</label>
+          <input type="password" name="password" />
+        </div>
+        <div className="inputField">
+          <input type="submit" value="Log In" />
+        </div>
+      </form>
+      <form className="logout" action="/logout">
+        <button type="submit">Logout</button>
+      </form>
     </div>
   );
 }
