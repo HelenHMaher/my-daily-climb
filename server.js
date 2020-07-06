4e11b874ed5da143fb6cc4ed9fef1df094f875ff
HelenHMaher <helenharlanmaher@gmail.com>
2020-07-06 14:19:51 +0200

    setting up backend routes

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
 const app = express();
-const apiRoutes = require(".src/routes/api.js");
-const auth = require(".src/routes/auth.js");
+const apiRoutes = require("./src/routes/api.js");
+const auth = require("./src/routes/auth.js");
 const session = require("express-session");
 const mongo = require("mongodb").MongoClient;
 
@@ -27,22 +27,26 @@ app.use(
   })
 );
 
-mongo.connect(process.env.MONGO_URI, (err, client) => {
-  let db = client.db("my-daily-climb");
-  if (err) {
-    console.log("Database err: " + err);
-  } else {
-    console.log("Successful database connection");
+mongo.connect(
+  process.env.MONGO_URI,
+  { useUnifiedTopology: true },
+  (err, client) => {
+    let db = client.db("my-daily-climb");
+    if (err) {
+      console.log("Database err: " + err);
+    } else {
+      console.log("Successful database connection");
 
-    auth(app, db);
-    apiRoutes(app, db);
+      auth(app, db);
+      apiRoutes(app, db);
 
-    app.use(function (req, res, next) {
-      res.status(404).type("text").send("Not Found");
-    });
+      app.use(function (req, res, next) {
+        res.status(404).type("text").send("Not Found");
+      });
 
-    app.listen(process.env.PORT || 3000, () =>
-      console.log("Server is running...")
-    );
+      app.listen(process.env.PORT || 3000, () =>
+        console.log("Server is running...")
+      );
+    }
   }
-});
+);
