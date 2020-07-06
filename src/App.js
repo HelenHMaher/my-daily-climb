afe10832b7f133b846406ad1b01ec69c85be8d70
HelenHMaher <helenharlanmaher@gmail.com>
2020-07-06 12:05:07 +0200

    need to call to backend for authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link, Redirect } from "react-router-dom";
 import Login from "./views/login";
+import Profile from "./views/profile";
+import Home from "./views/home";
 
 function App() {
+  const isAuthenticated = false;
+  const name = "John";
   return (
     <BrowserRouter>
       <header>My Daily Climb</header>
-      <Route path="/" component={Login} />
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to={`/profile/${name}`}>Login</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
+        {isAuthenticated ? (
+          <>
+            <Route path="/profile/:name" component={Profile} />
+          </>
+        ) : (
+          <Redirect to="/login" />
+        )}
+      </Switch>
     </BrowserRouter>
   );
 }
---
+++ src/views/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div>HOME PAGE</div>
+    </div>
+  );
+}
+
+export default Home;
