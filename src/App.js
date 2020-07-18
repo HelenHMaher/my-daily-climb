f546dec15d120d5904bfe14835adcbdcb1cbbd24
HelenHMaher <helenharlanmaher@gmail.com>
2020-07-18 17:11:56 +0200

    react router switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route, Link } from "react-router";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import Home from "./components/Home";
 import ClimbingSession from "./components/ClimbingSession";
@@ -10,33 +10,35 @@ import MyProfile from "./components/MyProfile";
 function App() {
   return (
     <div>
-      <h1>My Daily Climb</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/climbing-session">Log a Climbing Session</Link>
-          </li>
-          <li>
-            <Link to="/log-workout">Log a Workout</Link>
-          </li>
-          <li>
-            <Link to="/dryland-workouts">Dryland Training</Link>
-          </li>
-          <li>
-            <Link to="/my-profile">My Profile</Link>
-          </li>
-        </ul>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/climbing-session" component={ClimbingSession} />
-        <Route exact path="/log-workout" component={LogWorkout} />
-        <Route exact path="/dryland-workouts" component={DrylandWorkouts} />
-        <Route exact path="/my-profile" component={MyProfile} />
-      </nav>
       <Router>
-        <form class="logout" action="/logout">
+        <h1>My Daily Climb</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/climbing-session">Log a Climbing Session</Link>
+            </li>
+            <li>
+              <Link to="/log-workout">Log a Workout</Link>
+            </li>
+            <li>
+              <Link to="/dryland-workouts">Dryland Training</Link>
+            </li>
+            <li>
+              <Link to="/my-profile">My Profile</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/climbing-session" component={ClimbingSession} />
+            <Route exact path="/log-workout" component={LogWorkout} />
+            <Route exact path="/dryland-workouts" component={DrylandWorkouts} />
+            <Route exact path="/my-profile" component={MyProfile} />
+          </Switch>
+        </nav>
+        <form className="logout" action="/logout">
           <button type="submit">Logout</button>
         </form>
       </Router>
