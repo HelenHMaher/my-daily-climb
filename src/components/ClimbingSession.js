d9d97466fb5173e64ea765ea7ba6059d209ab0e2
HelenHMaher <helenharlanmaher@gmail.com>
2020-07-25 14:07:50 +0200

    climbing session form

--- src/components/ClimbingSession.js
+++ src/components/ClimbingSession.js
@@ -9,6 +9,24 @@ function ClimbingSession() {
         +I can add individual climbs within this session
         +above information is displayed and can be changed, deleted or updated
         --these climbing sessions can be displayed and searched in the profile section*/}
+
+      <form id="newBookForm" class="border">
+        <input
+          type="text"
+          id="bookTitleToAdd"
+          name="title"
+          placeholder="New Book Title"
+        />
+        <button type="submit" value="Submit" id="newBook">
+          Submit New Book!
+        </button>
+      </form>
+      <div id="display"></div>
+      <div id="bookDetail" class="border">
+        <p id="detailTitle">Select a book to see it's details and comments</p>
+        <ol id="detailComments"></ol>
+      </div>
+      <button id="deleteAllBooks">Delete all books...</button>
     </div>
   );
 }
