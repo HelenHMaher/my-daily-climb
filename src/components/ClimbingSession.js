c1ca75db0561a93e117338565785c8361a3bb1de
HelenHMaher <helenharlanmaher@gmail.com>
2020-07-27 10:15:48 +0200

    radio

--- src/components/ClimbingSession.js
+++ src/components/ClimbingSession.js
@@ -11,19 +11,15 @@ function ClimbingSession() {
         --these climbing sessions can be displayed and searched in the profile section*/}
 
       <form id="newSessionForm">
-        <input type="date" id="date" placeholder="Date" />
-        <input type="text" id="location" placeholde="Location" />
+        <input type="date" id="date" />
+        <input type="text" id="location" placeholder="Location" />
+        <input type="radio" id="indoor" name="indoorOutdoor" value="indoor" />
+        <label for="indoor">Indoor</label>
+        <input type="radio" id="outdoor" name="indoorOutdoor" value="outdoor" />
+        <label for="outdoor">Outdoor</label>
 
-        <button type="submit" value="Submit" id="newClimb">
-          Submit New Climb!
-        </button>
+        <button type="submit" value="Submit" id="newClimb" />
       </form>
-      <div id="display"></div>
-      <div id="climbDetail">
-        <p id="detailTitle">Select a climb to see it's details and comments</p>
-        <ol id="detailComments"></ol>
-      </div>
-      <button id="deleteClimb">Delete climb...</button>
     </div>
   );
 }
